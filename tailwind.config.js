3698a907a13d88f676a842219e72bf2119b06c80
SantiBavaresco <santiagobavaresco@gmail.com>
2023-06-04 14:56:14 -0300

    middle things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
         fontFamily:{
           burtons: "burtons",
+          poppins: ["Poppins", "sans-serif"],
         },
     },
   },
